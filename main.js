a8f61f7bd56c4e9036596fb10176b8880d3d7aed
ShreyashGedam <97446064+ShreyashGedam@users.noreply.github.com>
2022-03-03 18:25:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var adding = require("./add")
+
+var minus = require("./subtract")
+
+var multiply = require("./multiply")
+
+var divide = require("./divide")
+
+var x = 10
+var y = 5
+
+console.log(`Addition = ${adding(x,y)}`)
+
+console.log(`Subtraction = ${minus(x,y)}`)
+
+console.log(`Multiplication = ${multiply(x,y)}`)
+
+console.log(`Division = ${divide(x,y)}`)
